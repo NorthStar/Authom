a7db0251b938f9cddf1146dbeb0671e97337c976
Mimee Xu <xxu@hmc.edu>
2015-02-21 00:56:33 -0800

    Initial Commit

---
+++ Authom/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Authom
+//
+//  Created by Mimee Xu on 2/21/15.
+//  Copyright (c) 2015 Mimee Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AuthomTests/AuthomTests.m
@@ -0,0 +1,40 @@
+//
+//  AuthomTests.m
+//  AuthomTests
+//
+//  Created by Mimee Xu on 2/21/15.
+//  Copyright (c) 2015 Mimee Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface AuthomTests : XCTestCase
+
+@end
+
+@implementation AuthomTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
