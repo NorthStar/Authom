3b78b37ebe11006098f1347beb31677fcb3adae5
Mimee Xu <xxu@hmc.edu>
2015-02-22 01:54:04 -0800

    Integrated Firebase and a dumb chat system. Need to do some cleanup on UI.

--- Authom/THPaymentViewController.h
+++ Authom/THPaymentViewController.h
@@ -7,7 +7,17 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Firebase/Firebase.h>
 
-@interface THPaymentViewController : UIViewController
+@interface THPaymentViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) NSString* name;
+@property (nonatomic, strong) NSMutableArray* chat;
+@property (nonatomic, strong) Firebase* firebase;
+
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@property (weak, nonatomic) IBOutlet UIButton *nameField;
 
 @end
--- Podfile
+++ Podfile
@@ -5,5 +5,5 @@ pod 'SVProgressHUD'
 pod 'Venmo-iOS-SDK', '~>1.3'
 pod 'Braintree'
 pod 'AFNetworking', '~> 2.5'
-
+pod 'Firebase', '>= 2.2.0'
 
